27bb0f769f3e9e5a3836e0de50d2678634b356e2
Роман <romanze1706@gmail.com>
2022-11-11 21:20:45 +0300

    Задание 1.3

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,53 @@
+import java.time.Year;
+
 public class Main {
     public static void main(String[] args) {
         int deposit = 15000;
         int total = 0;
-        int m =1;
-           while ( total <2_459_000)
-           {
-            total = total + total /100;
+        int m = 1;
+        while (total < 2_459_000) {
+            total = total + total / 100;
             total = total + deposit;
             System.out.println("Месяц " + m++ + " сумма накоплений равна " + total + " рублей.");
-           }
+        }
 
-           int a =0;
-           while (a <=10) {
-             System.out.print(a ++ + " ");
-               System.out.print(a ++ + " ");
-               System.out.print(a ++ + " ");
-               System.out.print(a ++ + " ");
-               System.out.print(a ++ + " ");
-               System.out.print(a ++ + " ");
-               System.out.print(a ++ + " ");
-               System.out.print(a ++ + " ");
-               System.out.print(a ++ + " ");
-               System.out.print(a ++ + " ");
-               System.out.println(a ++);
+        int a = 0;
+        while (a <= 10) {
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.println(a++);
 
-           }
-           for (int b =10; b >=0;) {
-               System.out.print(b-- + " ");
-               System.out.print(b-- + " ");
-               System.out.print(b-- + " ");
-               System.out.print(b-- + " ");
-               System.out.print(b-- + " ");
-               System.out.print(b-- + " ");
-               System.out.print(b-- + " ");
-               System.out.print(b-- + " ");
-               System.out.print(b-- + " ");
-               System.out.print(b-- + " ");
-               System.out.println(b-- + " ");
+        }
+        for (int b = 10; b >= 0; ) {
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.println(b-- + " ");
+
+            int y = 12_000_000;
+            for (int year = 1; year <= 10; year++) {
+                System.out.println("Год " + year + " численность населения составляет " + y + " человек(ка)");
+                y = y + y * 9 / 1000;
+
+            }
 
 
-           }
+        }
 
 
     }
